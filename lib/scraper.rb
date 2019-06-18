cb364a1678faa9257be3620c9072e692d1ac1613
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-18 20:58:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,32 @@
 require 'nokogiri'
 require 'open-uri'
-
+require "pry"
 require_relative './course.rb'
 
 class Scraper
+  
+    def get_page
+      @@doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
+    end 
+    
+    def get_courses 
+      @@doc.css("#course-grid .post")
+     # binding.pry
+    end 
+    
+   # need title(h2), schedule(.date), and description(p)
+    
+    def make_courses
+      changer = 1
+      n = changer
+     get_courses.each do |inst|
+          n = Course.new
+          n.title = inst.css("h2").text
+          n.schedule = inst.css(".date").text
+          n.description = inst.css("p").text
+          changer += 1
+      end
+    end 
   
     def print_courses
     self.make_courses
@@ -14,7 +37,7 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
-  end
+    end
   
 end
 
